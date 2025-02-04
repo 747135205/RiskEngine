3c9b88df524d019583180edb6ca8abb6d6e5db0f
linqi <wkw4399xs@gmail.com>
2025-02-05 00:25:19 +0800

    feature:项目 common 通用模块异常枚举

---
+++ RiskEngine-commons/src/main/java/com/linqi/RiskCtrlSys/commons/exception/enums/RedisExceptionInfo.java
@@ -0,0 +1,24 @@
+package com.linqi.RiskCtrlSys.commons.exception.enums;
+
+import lombok.Getter;
+
+/**
+ * author: linqi
+ * description: Redis异常枚举类
+ * date: 2024
+ */
+
+@Getter
+public enum RedisExceptionInfo implements BizExceptionInfo  {
+
+    REDISTEMPLATE_NULL("-300", "RedisTemplate对象为null");
+
+    private String exceptionCode;
+    private String exceptionMsg;
+
+    RedisExceptionInfo(String exceptionCode,
+                       String exceptionMsg) {
+        this.exceptionCode = exceptionCode;
+        this.exceptionMsg = exceptionMsg;
+    }
+}
