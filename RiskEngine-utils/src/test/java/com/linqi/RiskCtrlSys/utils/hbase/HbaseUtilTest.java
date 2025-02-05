51b7295a333404a0c7659ee0e70e3c9ac952d603
linqi <wkw4399xs@gmail.com>
2025-02-06 00:03:33 +0800

    feature:项目工具类，HBase 相关工具类开发以及单元测试

---
+++ RiskEngine-utils/src/test/java/com/linqi/RiskCtrlSys/utils/hbase/HbaseUtilTest.java
@@ -0,0 +1,38 @@
+package com.linqi.RiskCtrlSys.utils.hbase;
+
+import com.linqi.RiskCtrlSys.utils.Application;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * author: linqi
+ * description: Hbase工具类单元测试
+ * date: 2024
+ */
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(classes = Application.class)
+public class HbaseUtilTest {
+
+    @Autowired
+    private HbaseUtil hbaseUtil;
+
+    @DisplayName("测试生成hbase Connection对象")
+    @BeforeAll
+    @Test
+    public void testGetConnection() throws Exception {
+        Connection conn = hbaseUtil.getConnection();
+        System.out.println(conn);
+    }
+
+    @DisplayName("测试生成hbase Admin对象")
+    @Test
+    public void testGetAdmin() throws Exception {
+        Admin admin = hbaseUtil.getAdmin();
+        System.out.println(admin);
+    }
+
+}
