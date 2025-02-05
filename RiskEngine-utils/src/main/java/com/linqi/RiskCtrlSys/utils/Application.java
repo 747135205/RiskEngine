c0418d680b7c6484677baff29b2a74a8906117a8
linqi <wkw4399xs@gmail.com>
2025-02-06 00:05:17 +0800

    feature:项目工具类模块，由于 HBase 单元测试需要启动相关的配置，所以需要配置一下启动类

---
+++ RiskEngine-utils/src/main/java/com/linqi/RiskCtrlSys/utils/Application.java
@@ -0,0 +1,17 @@
+package com.linqi.RiskCtrlSys.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author: linqi
+ * description: SpringBoot 启动类 (Hbase单元测试需要用到)
+ * date: 2024
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
